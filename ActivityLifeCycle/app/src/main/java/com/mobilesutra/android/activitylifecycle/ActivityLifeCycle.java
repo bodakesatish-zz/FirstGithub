db9b6ee503ea7f20c2383128a326ba4a9fe4369e
bodakesatish <bodakesatish@gmail.com>
2016-08-22 17:44:56 +0530

    Minor Changes Made
    Minor Changes Made

--- ActivityLifeCycle/app/src/main/java/com/mobilesutra/android/activitylifecycle/ActivityLifeCycle.java
+++ ActivityLifeCycle/app/src/main/java/com/mobilesutra/android/activitylifecycle/ActivityLifeCycle.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+/* Satish bodake */
 public class ActivityLifeCycle extends AppCompatActivity {
 
     String LOG_TAG = "LogActivityLifeCycle";
